d57f836538679be0748638d5722b5ce58f09cc9d
def1s <temnay2@icloud.com>
2024-05-12 13:12:12 +0300

    eleventh lab done

--- front/src/components/NavigationBar.jsx
+++ front/src/components/NavigationBar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faHome, faUser} from '@fortawesome/free-solid-svg-icons'
-import {Link, useNavigate} from 'react-router-dom';
+import {faBars, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Utils from "../utils/Utils";
 import BackendService from "../services/BackendService";
-import {connect, useDispatch} from "react-redux";
+import {connect} from "react-redux";
 import {userActions} from "../utils/Rdx";
 
 class NavigationBarClass extends React.Component {
 
     constructor(props) {
         super(props);
+
         this.goHome = this.goHome.bind(this);
         this.logout = this.logout.bind(this)
     }
-
 
     logout() {
         BackendService.logout()
@@ -26,16 +27,22 @@ class NavigationBarClass extends React.Component {
             })
     }
 
-
-
     goHome() {
         this.props.navigate('home');
     }
 
 
+
+
+
     render() {
         return (
             <Navbar bg="light" expand="lg">
+                <button type="button"
+                        className="btn btn-outline-secondary mr-2"
+                        onClick={this.props.toggleSideBar}>
+                    <FontAwesomeIcon icon={ faBars} />
+                </button>
                 <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}My RPO</Navbar.Brand>
                 <Navbar.Brand>myRPO</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -44,7 +51,8 @@ class NavigationBarClass extends React.Component {
                         {/*<Nav.Link href="/home">Home</Nav.Link>*/}
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
                         <Nav.Link onClick={this.goHome}>Another home</Nav.Link>
-                        <Nav.Link onClick={() => { this.props.history.push("/home")}} >Yet another home</Nav.Link>
+                        <Nav.Link onClick={() => { this.props.navigate("/home")}} >Yet another home</Nav.Link>
+                        <Nav.Link as={Link} to="/account">{this.props.user && this.props.user.login}</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 <Navbar.Text>{this.props.user && this.props.user.login}</Navbar.Text>
@@ -59,6 +67,13 @@ class NavigationBarClass extends React.Component {
     }
 
 
+
+
+
+
+
+
+
 }
 
 const NavigationBar = props => {
@@ -68,11 +83,10 @@ const NavigationBar = props => {
 }
 
 
-const mapStateToProps = state => {
+function mapStateToProps(state) {
+
     const { user } = state.authentication;
     return { user };
 }
 
 export default  connect(mapStateToProps)(NavigationBar);
-
-// export default  NavigationBar;
